262fc4846ad93e17115f18cd5c341cb0753a208f
driverdrift <kxktb@outlook.com>
2026-01-27 19:06:19 +0800

    Update choose_install_disk.sh

--- choose_install_disk.sh
+++ choose_install_disk.sh
@@ -28,6 +28,15 @@ choose_install_disk() {
 		ans=$(printf '%s' "$ans" | xargs)
 		[[ "$ans" =~ ^([Yy]|[Yy][Ee][Ss])$ ]] || exit 1
 	fi
+
+	echo -e "g\n					\
+		n\n\n\n+1M\nt\n4\n			\
+		n\n\n\n+100M\nt\n\nuefi\n	\
+		n\n\n\n+512M\n				\
+		n\n\n\n-2049M\n				\
+		n\n\n\n+1G\n				\
+		n\n\n\n\nt\n\nswap\n		\
+		w\n" | fdisk $target
 	
 	echo "$target"
 }
