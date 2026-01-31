475d60eace623a8e0739fec45b194e45e29b2479
driverdrift <kxktb@outlook.com>
2026-01-31 18:28:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ trap 'echo "operation is interrupted"; exit 130' INT
 REPO_URL="https://github.com/driverdrift/debian-install/archive/main.tar.gz"
 WORKDIR="/tmp/debian-install"
 
+skip_confirm=false
+if [[ "${1-}" =~ ^([Yy][Ee][Ss]|[Yy])$ ]]; then
+	skip_confirm=true
+fi
+
 rm -rf "$WORKDIR" && mkdir -p "$WORKDIR"
 
 command -v wget &>/dev/null || {
@@ -21,4 +26,4 @@ cd "$WORKDIR"
 echo "Done! Current directory: $(pwd)"
 
 chmod +x main.sh
-exec ./main.sh
+exec ./main.sh "$skip_confirm"
