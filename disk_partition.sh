1d5287d8b83589063de1d42d3cc075f1abdb3208
driverdrift <kxktb@outlook.com>
2026-02-18 22:12:56 +0800

    Update disk_partition.sh

--- disk_partition.sh
+++ disk_partition.sh
@@ -35,10 +35,10 @@ disk_partition() {
 	(
 		echo -e "g\n					\
 			n\n\n\n+1M\nt\n4\n			\
-			n\n\n\n+20M\nt\n\nuefi\n	\
-			n\n\n\n+512M\n				\
-			n\n\n\n-2049M\n				\
-			n\n\n\n+1G\n				\
+			n\n\n\n+15M\nt\n\nuefi\n	\
+			n\n\n\n+5M\n				\
+			n\n\n\n+1M\n				\
+			n\n\n\n+900M\n				\
 			n\n\n\n\nt\n\nswap\n		\
 			w\n"
 	) | fdisk $target 1>/dev/null
